f735757ec69208e223748d47f0c28bc030a4905d
jesusfortea <464186.joan23@fje.edu>
2025-03-27 13:56:48 +0100

    Estructura de la web

---
+++ index.php
@@ -0,0 +1,7 @@
+
+<?php
+
+session_start();
+header("Location: ./view/menu_page.php");
+?>
+
---
+++ view/menu_page.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Carta del día</title>
+</head>
+<body>
+    
+    <header>
+
+        <section>
+
+            <img id="header_img" src="../img/header_img.png" alt="">
+            <h1>Bar de la FAROLA</h1>
+
+        </section>
+
+
+    </header>
+
+
+</body>
+</html>
