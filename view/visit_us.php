45f34f34cdc8f106d1dc3b54b22a0442e7ee6e4e
jesusfortea <464186.joan23@fje.edu>
2025-04-14 23:41:10 +0200

    Página Visit Us completada, faltan animaciones, es una versión provisional

--- view/visit_us.php
+++ view/visit_us.php
@@ -14,6 +14,7 @@ if(isset($_SESSION['welcome']) && $_SESSION['welcome'] != 'check'){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Visitanos</title>
+    <link rel="stylesheet" href="../stylesheets/stylesheet.css">
 </head>
 <body>
     
@@ -21,27 +22,31 @@ if(isset($_SESSION['welcome']) && $_SESSION['welcome'] != 'check'){
 
         <section class="header_content_image">
 
-            <img src="" alt="">
+            <img src="../img/background_visit_us.png" alt="No se ha podido cargar la imagen">
         
         </section>
 
-        <section class="header_content_options">
+        <section class="visit_header_content_letter">
 
-            <article class="header_content_options_default">
-
-                <a href="./letter.php">Test1</a>
-
+            <h2>Visita nuestra carta</h2>
+            <article>
+                <p>Somos <strong>El bar de la <span class="farola">FAROLA</span></strong>. Garantizamos un gran servicio y sobretodo un buen sabor de boca, si desea comer hasta explotar y chupándose los dedos este es tu bar.</p>
             </article>
-
-            <article class="header_content_options_vip">
-
-                <a href="./vip.php">Test2</a>
-
-            </article>
+            
+            <a href="./letter.php">¡Entra ya!</a>
 
         </section>
 
+        <section class="visit_header_content_vip">
 
+            <h2><span class="farola">VIP</span></h2>
+            <article>
+                <p>¿Ya eres cliente <span class="farola">VIP</span> en nuestro bar? Inicia sesión para obtener ventajas de personalización, garantía de devolución por si no te ha gustado el servicio y mucho más.</p>
+                <a href="./vip.php">Iniciar sesión</a>
+            </article>
+            
+
+        </section>
 
 
     </header>
